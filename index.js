6e4eb48b3a3bbb5c51d81a05c815d8fb38783554
allfake <allfake24@gmail.com>
2018-06-23 15:07:14 +0700

    add config amazon

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 'use strict';
 
+const apiVersion = "/api/v1/";
+const noderedUrl = "http://localhost:1880/flows";
+const FormData = require('form-data');
 const path = require('path');
 const fs = require('fs');
 const Hapi = require('hapi');
 const basePath = __dirname;
+const noderedFilePath = path.join(basePath, "/nodered.json");
 const mqttFolder = path.join(basePath, "/mqtt");
 const mqttConfigFolder = path.join(basePath, "/mqttConfig");
+const amazonIotConfigFile = path.join(basePath, "/amazonIotConfig/config");
+const amazonIotCertsFolder = path.join(basePath, "/amazonIotConfig/certs");
+const request = require('request');
 
 // Create a server with a host and port
 const server = new Hapi.Server({  
-  host: 'localhost',
+  host: '0.0.0.0',
   port: 8000
 });
+
+
 
 // Add the route
 server.route(
@@ -23,11 +32,11 @@ server.route(
             additionalHeaders: ['cache-control', 'x-requested-with']
         }
     },
-    path: '/api/v1/mqtt/config/delete/{f_name}',
+    path: apiVersion + 'mqtt/config/delete/{f_name}',
     handler: function (request, reply) {
       if (fs.statSync(mqttFolder + "/" + request.params.f_name))
         {
-          deleteFile(request.params.f_name);
+          deleteMqttFile(request.params.f_name);
           return "Done";
         }
         else
@@ -47,7 +56,7 @@ server.route(
             additionalHeaders: ['cache-control', 'x-requested-with']
         }
     },
-    path: '/api/v1/mqtt/config',
+    path: apiVersion + 'mqtt/config',
     handler: function (request, reply) {
       const method = request.method
       const payload = request.payload
@@ -59,7 +68,7 @@ server.route(
         if (files.length == 0)
         {
           payload.config["f_name"] = "1";
-          writhFile("1", payload.config)
+          writhMqttFile("1", payload.config)
         }
         else 
         {
@@ -71,12 +80,12 @@ server.route(
 
             payload.config["f_name"] = newFileName.toString();
 
-            writhFile(newFileName, payload.config);
+            writhMqttFile(newFileName, payload.config);
           }
           else 
           {
             payload.config["f_name"] = "1";
-            writhFile("1", payload.config);
+            writhMqttFile("1", payload.config);
           }
         }
 
@@ -102,7 +111,7 @@ server.route(
       }
       else if (method === "patch") {
 
-        writhFile(payload.config["f_name"], payload.config);
+        writhMqttFile(payload.config["f_name"], payload.config);
 
         return payload;
       }
@@ -112,18 +121,170 @@ server.route(
     },
   });
 
-var writhFile = function(fileName, config)
+
+server.route(
+  {
+    method: ['GET'],
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    },
+    path: apiVersion + 'amazonIot/config/recommend',
+    handler: function (request, reply) {
+      const method = request.method;
+      const payload = request.payload;
+        
+      var recommendSetting = {}
+      recommendSetting.aws_certs = amazonIotCertsFolder;
+
+
+      var files = fs.readdirSync(amazonIotCertsFolder);
+
+      if (files.length != 0)
+      {
+        files.forEach(function(file) {
+
+          var fileStat = fs.statSync(path.join(amazonIotCertsFolder, file));
+
+          if (file.indexOf("root-CA.crt") !== -1)
+          {
+            recommendSetting.rootCA = fileStat.mtime;
+          }
+          else if (file.indexOf(".cert.pem") !== -1)
+          {
+            recommendSetting.cert = fileStat.mtime;
+          }
+          else if (file.indexOf(".private.key") !== -1)
+          {
+            recommendSetting.privateKey = fileStat.mtime;
+          }
+
+        });
+      }
+
+      return recommendSetting;
+
+    },
+  });
+
+
+server.route(
+  {
+    method: ['POST'],
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        },
+        payload: {
+            output: 'file',
+            allow: 'multipart/form-data',
+        }
+    },
+    path: apiVersion + 'amazonIot/config/certs/{cert_name}',
+    handler: function (request, reply) {
+
+      var certName = request.params.cert_name;
+
+      if (certName == "rootCA" || certName == "cert" || certName == "privateKey" && request.payload.cert.filename != '')
+      {
+        var certFilename = certName;
+
+        if (certFilename == "rootCA")
+        {
+          certFilename = "root-CA.crt";
+        }
+        else if (certFilename == "cert")
+        {
+          certFilename = request.payload.cert.filename + ".cert.pem";
+        }
+        else if (certFilename == "privateKey")
+        {
+          certFilename = request.payload.cert.filename + ".private.key";
+        }
+
+        writhAmazonCertFile(certFilename, request.payload.cert);
+
+        UpdateNodeRed();
+        return "Done";
+      }
+
+      return "Failed";
+
+    }
+  });
+
+server.route(
+  {
+    method: ['GET', 'PATCH'],
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    },
+    path: apiVersion + 'amazonIot/config',
+    handler: function (request, reply) {
+      const method = request.method
+      const payload = request.payload
+      var responseData = [];
+      if (method === "post") {
+
+        writhAmazonIotFile(payload.config);
+
+        UpdateNodeRed();
+        return payload;
+
+      }
+      else if (method === "get") {
+
+        var content = fs.readFileSync(amazonIotConfigFile, "utf8");
+        content = content.replace(/(?:\r\n|\r|\n)/g, '');
+
+        return content;
+      }
+      else if (method === "patch") {
+        writhAmazonIotFile(payload.config);
+
+        UpdateNodeRed();
+        return payload;
+      }
+      else {
+        return "Error";
+      }
+    },
+  });
+
+
+var writhMqttFile = function(fileName, config)
 {
   fileName = fileName.toString();
   fs.writeFileSync(path.join(mqttFolder, fileName),  JSON.stringify(config));
   fs.writeFileSync(path.join(mqttConfigFolder, "config_" + fileName + ".conf"),  configTemplete(config));
 }
 
-var deleteFile = function(fileName)
+var deleteMqttFile = function(fileName)
 {
   fileName = fileName.toString();
   fs.unlinkSync(mqttFolder + "/" + fileName);
   fs.unlinkSync(mqttConfigFolder + "/config_" + fileName + ".conf");
+}
+
+var writhAmazonIotFile = function(config)
+{
+  fs.writeFileSync(amazonIotConfigFile,  JSON.stringify(config));
+}
+
+var deleteAmazonIotFile = function()
+{
+  fs.unlinkSync(amazonIotConfigFile);
+}
+
+var writhAmazonCertFile = function(fileName, data)
+{
+  fs.renameSync(data.path, path.join(amazonIotCertsFolder, fileName))
 }
 
 var configTemplete = function(config) {
@@ -150,6 +311,7 @@ var configTemplete = function(config) {
 
   return templete;
 }
+
 // Start the server
 server.start((err) => {
 
@@ -158,3 +320,49 @@ server.start((err) => {
   }
   console.log('Server running at:', server.info.uri);
 });
+
+var UpdateNodeRed = function() {
+  request(noderedUrl, function (error, response, body) {
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    
+    body = JSON.parse(body);
+
+    for (var i = 0; i < body.length; i++)
+    {
+      if (body[i]['type'] == "aws-iot-device")
+      {
+        var readAmazonFile = fs.readFileSync(amazonIotConfigFile);
+        readAmazonFile = JSON.parse(readAmazonFile);
+        
+        body[i]['clientId'] = readAmazonFile.client_id;
+        body[i]['endpoint'] = readAmazonFile.endpoint;
+        body[i]['awscerts'] = readAmazonFile.aws_certs;
+      }
+      else if (body[i]['type'] == "aws-mqtt out" || body[i]['type'] == "aws-mqtt in") 
+      {
+        body[i]['topic'] = readAmazonFile.topic;
+      }
+    }
+
+    request.post(
+      {
+        url: noderedUrl, 
+        body: JSON.stringify(body),
+        headers: {
+          'Content-type': 'application/json',
+          'Node-RED-Deployment-Type': 'full',
+        },
+      }, 
+      function optionalCallback(err, httpResponse, body) {
+        
+        if (err) {
+          return console.error('upload failed:', err);
+        }
+        console.log('Upload successful!  Server responded with:', body);
+
+      }
+    );
+
+  });
+
+}
